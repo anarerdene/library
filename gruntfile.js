3364ba7238804e42906e196d047d26231ae3d8ca
Anar-Erdene <b.anarerdene@gmail.com>
2017-03-30 22:20:30 +0900

    grunt sass compiler

--- gruntfile.js
+++ gruntfile.js
@@ -2,19 +2,31 @@ module.exports = function(grunt){
 
     grunt.initConfig({
 
-      sass: {
-        dist: {
-          options: {
-            style: 'expanded'
+        sass: {
+            dist: {
+              options: {
+                style: 'expanded'
+              },
+              files: {
+                'dist/css/style.css': 'src/scss/main.scss',
+              }
+            }
+        },
+
+        watch: {
+          sass: {
+            files: ['src/scss/*.scss'],
+            tasks: ['sass'],
+            options: {
+              spawn: false,
+            },
           },
-          files: {
-            'dist/css/style.css': 'src/scss/main.scss',
-          }
-        }
-      }
+        },
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
     grunt.registerTask('default', ['sass']);
 };
