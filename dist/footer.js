89a630902c860ccfa86bfd338a073acb09d32601
Anar-Erdene <b.anarerdene@gmail.com>
2017-04-10 18:37:01 +0900

    removed unnecessary files

--- dist/footer.js
+++
@@ -1,8 +0,0 @@
-
-</body>
-</html>
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
